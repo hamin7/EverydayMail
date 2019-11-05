a227bab73895814e8c99433a740cf85fb6f93a34
hamin7 <ggamini7@gmail.com>
2019-11-06 02:10:57 +0900

    remove button
    add removal button

--- js/crud.js
+++ js/crud.js
@@ -3,6 +3,10 @@ function doFirst() {
     var button = document.getElementById("button");     // button 지역변수에 html내의 id가 button인 요소 대입.
     button.addEventListener("click", saveData, false)   // button에 리스너 - click 이벤트 발생 시 saveData() 실행.
     display();      // 잘 되는지 실험을 위해. 다른 사이트 나갔다가 돌아오는 경우를 대비해서.
+
+    var button_delete = document.getElementById("button_delete");
+    button_delete.addEventListener("click", deleteData, false)
+    display();
 }
 
 function saveData() {
@@ -17,6 +21,20 @@ function saveData() {
 
     document.getElementById('myform_title').value="";     // myform_title의 value를 빈칸으로 하기.
     document.getElementById('myform').value="";     // myform의 value를 빈칸으로 하기.
+}
+
+function deleteData() {
+    var myform = document.getElementById("myform").value;
+    var myform_title = document.getElementById("myform_title").value;
+
+    myform = myform.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+
+    localStorage.removeItem(myform_title, myform);
+
+    display();
+
+    document.getElementById('myform_title').value="";
+    document.getElementById('myform').value="";
 }
 
 function display() {
