2f973fba86cae78d3dfc815dd938634e8c17e180
suhyun1 <suhyun7079@gmail.com>
2019-11-05 21:23:13 +0900

    페이지 전환 방지 코드 추가
    사이드바로 드래그앤 드롭시 (의도하지 않은) 페이지 전환 방지를 위한 코드

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 
 $(document).ready(function() {
  
-    // 템플릿 추가 버튼 mouseover/mouseout시 event
-    document.getElementById("write_new").onmouseover = function () {
-        this.src = "icons/plus_light_icon_64.png"; 
-    };
-    
-    document.getElementById("write_new").onmouseout = function () {
-        this.src = "icons/plus_icon_64.png";
-    };
 
-   
+});
 
-})
+$("#write_new").mouseover(function () {
+    $(this).prop('src', 'icons/plus_light_icon_64.png');
+});
+$("#write_new").mouseout(function () {
+    $(this).prop('src', 'icons/plus_icon_64.png');
+});
 
 // checkbox 전체 선택 및 해제 
 $("#selectAll").click(function () {
@@ -35,8 +32,10 @@ $(".chk").click(function () {
 });
 
 
-// 편집한 내용 보여주기
+
 $("#btn_copy").hide();
+$("#btn_NaverMail").hide();
+// 편집한 내용 보여주기
 $("#btn_complete").click(function(){
     var written_str = "";
     var line_id;
@@ -51,6 +50,7 @@ $("#btn_complete").click(function(){
     $("#btn_cancel").hide();
     $("#result").show();
     $("#result").html(written_str);
+    $("#btn_NaverMail").show();
     $("#btn_copy").show();
     $(".content_description").text("마지막으로 다듬어보세요!");
 
@@ -80,11 +80,21 @@ function copyToClipboard(val){
         document.execCommand('copy');
         
     } catch (e) {
-        console.log("document.execCommand('copy'); 를 지원하지 않는 브라우저입니다.");
+        console.log("document.execCommand('copy');를 지원하지 않는 브라우저입니다.");
     }
-    
     document.body.removeChild(text);
 }
 
 
+//의도하지 않은 페이지 전환 방지
+window.addEventListener(`dragover`, (evt = event) => {
+    evt.preventDefault();
+    evt.dataTransfer.effectAllowed = `none`;
+    evt.dataTransfer.dropEffect = `none`;
+}, false);
 
+window.addEventListener(`drop`, (evt = event) => {
+    evt.preventDefault();
+    evt.dataTransfer.effectAllowed = `none`;
+    evt.dataTransfer.dropEffect = `none`;
+}, false);
