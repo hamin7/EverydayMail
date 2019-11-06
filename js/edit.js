92e84819dae111aa7bc058d754e344ad056bdd86
hamin7 <ggamini7@gmail.com>
2019-11-07 02:52:44 +0900

    Update edit.js
    not completed

--- js/edit.js
+++ js/edit.js
@@ -1,17 +1,31 @@
 $(document).ready(function () {
     // 새로 저장 버튼으로 새로운 myform 만들기.
     $('#btn_save').click(function () {
+        
+        saveElement();
         //location.href="write.html";
         location.replace("write.html");
         //location.reload();        // 새로고침
         
+        loadElement();
         //$("#myform").html("애용");
         //$(document).ready(function() { alert("첫번째 ready()"); });
-        $(document).ready(function() {
-            $("#myform").html(written_str);     // write.html의 myform textarea에 written_str 데이터 출력.
-            alert(written_str);
-        });
+        //$(document).ready(function() {
+        $("#myform").html(written_str);     // write.html의 myform textarea에 written_str 데이터 출력.
+        //alert(written_str);
+        //});
         //alert(written_str);
         //$("#myform").html(written_str);
     });
 });
+
+function saveElement() {
+    sessionStorage.setItem('myform', written_str)
+    alert("저장완료");
+}
+
+function loadElement() {
+    sessionStorage.getItem('myform')
+    alert("로드완료");
+}
+
