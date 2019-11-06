0658f838d8073759607ffcec49fc6c4fdfa6cb8e
hamin7 <ggamini7@gmail.com>
2019-11-06 21:32:14 +0900

    fafssdaf

--- js/main.js
+++ js/main.js
@@ -87,9 +87,3 @@ window.addEventListener(`drop`, (evt = event) => {
     evt.dataTransfer.effectAllowed = `none`;
     evt.dataTransfer.dropEffect = `none`;
 }, false);
-
-// 새로 저장 버튼으로 새로운 myform 만들기.
-$('#btn_save').click(function () {
-    location.href="write.html";
-    $("#myform").html(written_str);
-})
