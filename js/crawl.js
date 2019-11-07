7548bb60cae806df777754dc9245e0cac82eee07
hamin7 <ggamini7@gmail.com>
2019-11-08 02:29:04 +0900

    update crawl.js

--- js/crawl.js
+++ js/crawl.js
@@ -9,32 +9,34 @@ $(document).ready(function() {
 
 
 function download_get() {   // 이미 세팅된 네이버 모바일의 주소를 가져옴 
-	var str_url = $('#input_url').val();    // 네이버 모바일의 소스를 가져와 textArea에 삽입 
+	var str_url = $('#input_url').val();    // 
 	var jqxhr = $.get(str_url, function(data) {
 		$('#textvw').val(data);
 	});
 }
 
-function download_post() {      // 이미 세팅된 사이트의 주소를 가져옴 
-	var str_url = $('#input_url').val();    // 사이트의 소스를 가져와 textArea에 삽입 
+/*
+function download_post() {      // 
+	var str_url = $('#input_url').val();    // 
 	var jqxhr = $.post(str_url, function(data) {
 		$('#textvw').val(data);
 	});
 }
+*/
 
 $(document).ready(function() {
-	$('button').click(function() {  // 이름을 주지 않았기 때문에 index순서에 의해서 값을 가져옴 
-		switch($('button').index(this)) {
-		case 0:
-			download_get();
-			break;
-		case 1:
-			download_post();
-			break;
-		}
+	$('#btn_get').click(function() {
+		download_get();
 	});
 });
 
+/*
+$(document).ready(function() {
+	$('#btn_post').click(function() {
+		download_post();
+	});
+});
+*/
 
 function download() {        
     var str_url = $('#input_url').val();
